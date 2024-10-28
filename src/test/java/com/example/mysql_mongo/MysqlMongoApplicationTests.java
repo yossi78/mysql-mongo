16d11e228bcdef4486738611122ee994fba07bdf
yossi78 <yossi78@gmail.com>
2024-10-28 19:27:47 +0200

    First commit

---
+++ src/test/java/com/example/mysql_mongo/MysqlMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysql_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
