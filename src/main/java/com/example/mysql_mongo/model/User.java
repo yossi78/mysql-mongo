a2e9f9e63bfc4b4dccdbf0af2774bf46d0e754f3
yossi78 <yossi78@gmail.com>
2024-10-28 19:33:05 +0200

    add keys and model

---
+++ src/main/java/com/example/mysql_mongo/model/User.java
@@ -0,0 +1,22 @@
+package com.example.mysql_mongo.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int age;
+    private String firstName;
+    private String lastName;
+
+}
