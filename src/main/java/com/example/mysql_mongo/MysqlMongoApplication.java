6295048cf2da3fb11f77083b34716bf053964d6e
yossi78 <yossi78@gmail.com>
2024-11-24 13:46:48 +0200

    First commit

---
+++ src/main/java/com/example/mysql_mongo/MysqlMongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysql_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlMongoApplication.class, args);
+	}
+
+}
