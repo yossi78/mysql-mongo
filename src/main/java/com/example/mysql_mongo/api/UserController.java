9c078e4220dd897af5d2f3a95357eee7b846379b
yossi78 <yossi78@gmail.com>
2024-11-30 20:48:47 +0200

    add postman collection

--- src/main/java/com/example/mysql_mongo/api/UserController.java
+++ src/main/java/com/example/mysql_mongo/api/UserController.java
@@ -16,7 +16,6 @@ import java.util.List;
 public class UserController {
     private final UserService userService;
 
-
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
